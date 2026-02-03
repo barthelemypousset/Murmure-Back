d42ed012191ceab9976aebc435a021ee087e3a21
barthelemy pousset <barthelemypousset@gmail.com>
2026-02-03 22:24:04 +0100

    Version route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Murmure-back' });
 });
 
+router.get('/version', function (req, res, next) {
+  res.json({ result: true, version: "1.0" });
+});
+
 module.exports = router;
